4630ed8df356e61efd7f3de86397316cc17c50e9
robical <robi.calandrini@gmail.com>
2017-12-15 02:01:10 +0100

    Validation methods

---
+++ validation.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Validation Module:
+
+Methods available:
+    - Confusion Matrix
+    - Accuracy plot
+    
+This module estimates the prediction performance of the proposed algorithms, 
+using two well-known KPIs:
+	- Confusion matrix: this is a table resembling 		
+
+INPUT: Test set - raw data
+OUTPUT: Predicted class
+
+@author: robi
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap, vmin=0.0, vmax=1.0)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
